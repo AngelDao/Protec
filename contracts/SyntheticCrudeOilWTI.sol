13facefea4bc17586e41d24f39e717d146b97e31
Tiago Neto <tiago.f.neto@gmail.com>
2021-10-22 23:05:14 +0100

    burnable

--- contracts/SyntheticCrudeOilWTI.sol
+++ contracts/SyntheticCrudeOilWTI.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract SyntheticCrudeOilWTI is ERC20, Ownable {
+contract SyntheticCrudeOilWTI is ERC20Burnable, Ownable {
     constructor() ERC20("Synthetic Crude Oil WTI", "SCOWTI") {}
 
     function mint(address _addr, uint _amount) public onlyOwner {
