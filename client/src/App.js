3a837701e2bd4b02c2d1cf4047f357ad6aa3bc67
DanWizard <tortortorented@gmail.com>
2021-10-22 16:01:11 -0500

    basic client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
-}
+import React from "react";
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "./containers/Layout";
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <Layout />
+    </ChakraProvider>
+  );
+};
 
 export default App;
