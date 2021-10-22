b161e6458ab24689ab0dc0e4368dfe426d14368f
Tiago Neto <tiago.f.neto@gmail.com>
2021-10-22 21:34:46 +0100

    dummy token

---
+++ contracts/SyntheticCrudeOilWTI.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SyntheticCrudeOilWTI is ERC20, Ownable {
+    constructor() ERC20("Synthetic Crude Oil WTI", "SCOWTI") {}
+
+    function mint(address _addr, uint _amount) public onlyOwner {
+	    _mint(_addr, _amount);
+    }
+}
