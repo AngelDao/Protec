765be3b8bfbcbec0a0cd08d66cbd4406fb1fd781
meirbank <meir@meirbank.com>
2021-10-23 21:49:46 +0100

    helpers

--- client/src/helpers/approve.js
+++ client/src/helpers/approve.js
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers'
 
-export const approve = async (tokenContract, spenderContract, account) => {
-  return tokenContract.methods
+export const approve = async (tokenContract, spenderContract) => {
+  return await tokenContract.methods
     .approve(spenderContract.options.address, ethers.constants.MaxUint256)
-    .send({ from: account })
 }
