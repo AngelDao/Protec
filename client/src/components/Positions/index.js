6d11913f540af476ac87e820685a9279e78ffcfb
DCota <32775237+DaigaroCota@users.noreply.github.com>
2021-10-23 20:56:06 -0400

    Update index.js

--- client/src/components/Positions/index.js
+++ client/src/components/Positions/index.js
@@ -8,6 +8,7 @@ import {
 } from "./styles";
 import Dropdown from "../Dropdown";
 import optionsMetaData from "../../utils/optionsMetaData";
+import  getErc20Contract  from "../helpers/getContract";
 
 const Positions = () => {
   const { account, handleConnectWallet, switchModal } =
@@ -31,7 +32,31 @@ const Positions = () => {
 
   useEffect(() => {
     const opts = getOption()
-    const provisions = getProvisions()
+
+    const provisions = async() => {
+      let userProvisions =[];
+      for(let i=0; i < optionAMMPoolcontracts.length; i++) {
+        let tempDepositSnapshot = await optionAMMPoolcontracts[i].getUserDepositSnapshot();
+        if(tempDepositSnapshot[0] != 0 && tempDepositSnapshot[1] !=0 ) {
+          let assetAaddress = await optionAMMPoolcontracts[i].tokenA();
+          let assetA = await getErc20Contract(assetAaddress, account);
+          let assetAName = await assetA.name();
+          userProvisions.push({
+            optionName:`Option AMM Pool ${assetAName}`,
+            optionABal:tempDepositSnapshot[0],
+            optionBBal:tempDepositSnapshot[1],
+
+          });
+        }
+        if(userProvisions.length >0) {
+          return userProvisions;
+        } else {
+          return 0;
+        }
+        
+      }
+
+    };
   })
 
   return (
